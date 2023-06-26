549304d8e7b6e905f7c0cddb735f8accec781318
Mayank Handa <109039458+m13hcs@users.noreply.github.com>
2023-06-26 22:15:29 +0530

    Time: 0 ms (100%), Space: 5.9 MB (96.69%) - LeetHub

--- 0319-bulb-switcher/0319-bulb-switcher.cpp
+++ 0319-bulb-switcher/0319-bulb-switcher.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int bulbSwitch(int n) {
+        /*
+        Only the bulbs at square positions will be left on as they have
+        odd number of prime factories, implying that the last switch will 
+        turn the bulb on and there exists sqrt(n) squares in n integers
+        */
         
         return sqrt(n);
        
