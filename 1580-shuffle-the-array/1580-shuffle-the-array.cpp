20cb4c62796ca2ae863f9f139ffcfdb9a2277365
Mayank Handa <109039458+m13hcs@users.noreply.github.com>
2023-06-28 16:04:48 +0530

    Time: 7 ms (50.85%), Space: 9.8 MB (14.29%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.cpp
+++ 1580-shuffle-the-array/1580-shuffle-the-array.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
+      //Creating a vector and adding alternating elements of nums vector with the difference of 'n'
+      
         vector<int> shuffled_nums;
 
         for(int i = 0; i<n; i++)
