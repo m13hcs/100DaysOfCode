7020cc1b1aa762a38be3a668b6c1ca0af74245d6
Mayank Handa <109039458+m13hcs@users.noreply.github.com>
2023-07-02 23:19:48 +0530

    Time: 14 ms (49.05%), Space: 7.3 MB (80.97%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -4,18 +4,10 @@ public:
         
         if(s.length()==t.length())
         {
-            vector<char> vs(s.begin(),s.end());
-            vector<char> vt(t.begin(),t.end());
-            sort(vs.begin(),vs.end());
-            sort(vt.begin(),vt.end());
-            int ans = 0;
-            for(int i = 0; i<vt.size(); i++)
-            {
-                if(vs[i]==vt[i]){
-                    ans++;
-                }
-            }
-            if(ans==vs.size())
+            sort(s.begin(),s.end());
+            sort(t.begin(),t.end());
+           
+            if(s==t)
             {
                 return true;
             }
