8499a1ce46f6870b5dde129346816a60f1c68ce9
Mayank Handa <109039458+m13hcs@users.noreply.github.com>
2023-07-08 00:32:21 +0530

    Time: 46 ms (38.45%), Space: 13.1 MB (65.64%) - LeetHub

---
+++ 2508-maximum-sum-of-an-hourglass/2508-maximum-sum-of-an-hourglass.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int maxSum(vector<vector<int>>& grid) {
+
+        //time complexity: (rows-2)*(coloumns-2)
+        int ans = 0;
+        int rows = grid.size();
+        int coloumns = grid[0].size();
+
+        for(int i=0;i<rows-2;i++)
+        {
+            for(int j=0;j<coloumns-2;j++)
+            {
+                ans = max(ans,grid[i][j]+grid[i][j+1]+
+                              grid[i][j+2]+grid[i+1][j+1]+
+                              grid[i+2][j]+grid[i+2][j+1]+
+                              grid[i+2][j+2]);
+            }
+        }
+        return ans;
+    }
+};
